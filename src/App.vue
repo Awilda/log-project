1829dcc9acf1b39a278197bc0b1ecc940d721a59
Awilda <awildacouverthier@gmail.com>
2021-09-09 10:20:55 -0400

    add getBooks method

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,61 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    data() {
+      return {
+        books: []
+      };
+    },
+    async created() {
+     this.books = await this.getBooks();
+   },
+   methods: {
+    getBooks: async () => {
+      const query = `{
+        bookCollection {
+          items {
+            sys {
+              id
+            }
+            title
+            author
+            illustrator
+            publisher {
+              publisher
+              city
+            }
+          }
+        }
+      }`;
+
+      const fetchUrl = `https://graphql.contentful.com/content/v1/spaces/${process.env.VUE_APP_CONTENTFUL_SPACE_ID}`;
+      const fetchOptions = {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${process.env.VUE_APP_CONTENTFUL_ACCESS_TOKEN}`,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ query })
+      };
+
+        try {
+          const response = await fetch(fetchUrl, fetchOptions).then(response =>
+            response.json()
+            );
+          return response.data.bookCollection.items;
+        } catch (error) {
+          throw new Error("Could not receive the data from Contentful!");
+        }
+      }
+    }
+  };
+
 </script>
 
 <style>
