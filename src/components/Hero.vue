b9606e93d3c3c898556ff90a9a5a44a0ff41816e
Awilda <awildacouverthier@gmail.com>
2021-10-08 14:14:51 -0400

    add Hero Component and toggle functionality

---
+++ src/components/Hero.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 style="font-family:ubuntu">Pia's Books</h1>
+        <toggle-button :labels="{checked: 'Yes', unchecked: 'No'}" style="margin-left: 20px" /> 
+    </div>
+  </div>
+</template>
+  
+<script>
+export default {
+  data(){
+    return {
+      file: ''
+    }
+  },
+}
+
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Hero from 'vue-js-toggle-button'
 
 Vue.config.productionTip = false
+Vue.use(Hero)
+
 
 new Vue({
   render: h => h(App),
